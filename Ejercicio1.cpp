25dfe1ead8db2b1a705a7d56e4af02e61b6eae2b
Xavier00014624 <164245307+Xavier00014624@users.noreply.github.com>
2024-06-03 12:34:37 -0600

    Ejercicio

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -29,20 +29,29 @@ double division(double n1, double n2)
 
     return resultado;
 }
+double media(double n1, double n2)
+{
+    double resultado = 0;
+    resultado = (n1 + n2) / 2;
+
+    return resultado;
+}
 int main()
 {
-    double n1, n2, respuesta, respuesta2, respuesta3, respuesta4;
+    double n1, n2, respuesta, respuesta2, respuesta3, respuesta4, respuesta5;
     n1 = 5;
     n2 = 1;
     respuesta = suma(n1, n2);
     respuesta2 = resta(n1, n2);
     respuesta3 = multiplicacion(n1, n2);
     respuesta4 = division(n1, n2);
+    respuesta5 = media(n1, n2);
 
-    cout << "El resultado de la suma es " << respuesta<<"\n";
-    cout << "El resultado de la resta es " << respuesta2<<"\n";
-    cout << "El resultado de la multiplicacion es " << respuesta3<<"\n";
-    cout << "El resultado de la division es " << respuesta4<<"\n";
+    cout << "El resultado de la suma es " << respuesta << "\n";
+    cout << "El resultado de la resta es " << respuesta2 << "\n";
+    cout << "El resultado de la multiplicacion es " << respuesta3 << "\n";
+    cout << "El resultado de la division es " << respuesta4 << "\n";
+    cout << "El resultado de la media es " << respuesta5 << "\n";
 
     return 0;
 }
